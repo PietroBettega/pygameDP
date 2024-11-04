cc089ec3147bca31a7b1340cf56d5f3a0fa868a3
João Pedro Hugues <joaopmh@al.insper.edu.br>
2024-11-04 11:16:26 -0300

    aplicando os personagens

--- v1.py
+++ v1.py
@@ -24,10 +24,29 @@ clock=py.time.Clock()
 
 #Criando personagens
 class Personagem:
-    def __init__(self,nome,posicao_x,posicao_y):
+    def __init__(self,nome,nome_imagem, posicao_x,posicao_y):
         self.nome = nome
+        self.nome_imagem = nome_imagem
         self.posicao_x = posicao_x
         self.posicao_y = posicao_y
+
+#Aplicando os personagens
+
+personagem_nightborne = ('NightBorne','NightBorne',100, 300)
+personagem_demon = ('Demon','boss_demon_slime_FREE_v1.0', 600, 300)
+
+
+
+#Funções do jogo
+
+def limpa_screen():
+    imagem_fundo=py.image.load("PyGame Fundo.webp")
+    imagem_fundo=py.transform.scale(imagem_fundo,(WIDTH,HEIGHT))
+    screen.blit(imagem_fundo,(0,0))
+
+    personagem_imagem = py.image.load(r'')
+    screen.blit(personagem_imagem, (100,300))
+
 
 
 #Funções do jogo
