64884c7ddb764f53455c632cef6c4272885e9433
EnricoVBerto <enricovberto@hotmail.com>
2024-11-06 09:22:55 -0300

    Criando configs

---
+++ config.py
@@ -0,0 +1,16 @@
+import pygame as py
+
+
+#Tamanho da tela
+
+WIDTH=1200
+HEIGHT=700
+
+#Clock
+
+FPS=30
+clock=py.time.Clock()
+
+#Abrindo a Tela e Música
+
+screen=py.display.set_mode((WIDTH,HEIGHT))
