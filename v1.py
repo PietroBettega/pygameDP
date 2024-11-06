f056122a03a257b89f5509fe29d5c702673a39bc
Pietro <pietropb3@al.insper.edu.br>
2024-11-06 11:00:28 -0300

    ver

--- v1.py
+++ v1.py
@@ -58,7 +58,7 @@ for i in range(1,13):
         (768,448)))
 mon_images['walking'] = img_list
 for i in range(1,8):
-    img_list.append(py.transform.scale(py.image.load(f"assets/Mon batendo/1atk/1atk_{i}.png").convert_alpha(),
+    img_list.append(py.transform.scale(py.image.load(f"assets/Mon batendo1/1atk/1atk_{i}.png").convert_alpha(),
         (768,448)))
 mon_images['beating'] = img_list
 img_list = []
@@ -87,6 +87,7 @@ class Personagem:
         self.posicao_x = posicao_x
         self.posicao_y = posicao_y
         self.state = 'idle'
+        self.x_speed=0
         self.current_image = 1
         self.last_img_change = 0
 
@@ -113,6 +114,14 @@ class Personagem:
             self.last_img_change = 0
             self.current_image = (self.current_image + 1) % len(mon_images[self.state])
 
+    #Atualiza posição
+    self.posicao_x= self.posicao_x + self.x_speed
+    if self.posicao_x < 0:
+        self.posicao_x=0
+        self.state = 'idle'
+    if self.posicao_x > 950:
+        self.posicao_x = 950
+        self.state = 'idle'
 #Aplicando os personagens
 
 mon = Personagem('Monstro','idle_1',-250, 200)
@@ -136,13 +145,33 @@ while game:
     for event in py.event.get():
         if event.type == py.QUIT:
             game=False
-        # if event.type == py.KEYDOWN:
-        #     #se apertou esquerda
-        #     demon.state = 'walking'
-        #     demon.current_image = 0
-        # if event.type == py.KEYUP:
-        #     demon.state = 'idle'
-        #     demon.current_image = 0
+        if event.type == py.KEYDOWN:
+            #Direita
+            if event.type == py.K_d:
+                demon.state = 'walking'
+                demon.x_speed=10
+                demon.current_image = 0
+            if event.type ==py.K_l:
+                mon.state = 'walking'
+                mon.x_speed=-10
+                mon.current_image = 0
+            #Esquerda
+            if event.type == py.K_a:
+                demon.state = 'walking'
+                demon.x_speed=-10
+                demon.current_image = 0
+            if event.type ==py.K_j:
+                mon.state = 'walking'
+                mon.x_speed=10
+                mon.current_image = 0
+
+        if event.type == py.KEYUP:
+            if event.type == py.K_d or py.K_a:
+                demon.state = 'idle'
+                demon.current_image = 0
+            if event.type == py.K_l or py.K_j:
+                mon.state = 'idle'
+                mon.current_image = 0
 
     demon.update_demon()
     mon.update_mon()
